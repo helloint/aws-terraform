e09a6a56f894f4365784a5eda0143c9f0fe8e912
Wayne Mao <helloint@qq.com>
2022-11-17 15:47:51 +0800

    add schedule

--- main.tf
+++ main.tf
@@ -28,16 +28,20 @@ variable "lambda_name" {
   default = "persistWNBADataTF"
 }
 
+variable "schedule" {
+  default = "cron(0/10 * ? * MON-FRI *)"
+}
+
 provider "aws" {
   region = var.region
 }
 
 locals {
-  bucket_name           = var.bucket_name
-  lambda_name           = var.lambda_name
-  function_name         = "persistWNBAData"
-  zip_file_name         = "${path.module}/deployables/persistWNBAData.zip"
-  handler_name          = "persistWNBAData.handler"
+  bucket_name   = var.bucket_name
+  lambda_name   = var.lambda_name
+  function_name = "persistWNBAData"
+  zip_file_name = "${path.module}/deployables/persistWNBAData.zip"
+  handler_name  = "persistWNBAData.handler"
 }
 
 module "lambda_bucket" {
@@ -63,6 +67,25 @@ module "persist_wnba_data_lambda" {
   role_arn         = module.lambda_role.lambda_role_arn
   zip_filename     = local.zip_file_name
   zip_filehash     = data.archive_file.lambda_archive.output_base64sha256
+}
+
+resource "aws_cloudwatch_event_rule" "schedule" {
+  name                = "tf_wnba_schedule"
+  description         = "Schedule for Lambda Function"
+  schedule_expression = var.schedule
+}
+
+resource "aws_cloudwatch_event_target" "schedule_lambda" {
+  rule      = aws_cloudwatch_event_rule.schedule.name
+  target_id = "persist_wnba_data_lambda"
+  arn       = module.persist_wnba_data_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_events_bridge_to_run_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.persist_wnba_data_lambda.function_name
+  principal     = "events.amazonaws.com"
 }
 
 output "lambda_name" {
